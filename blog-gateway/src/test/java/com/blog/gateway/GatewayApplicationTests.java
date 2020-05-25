b86eda0c62d0f2156b745455aba2c45f0847a33f
meta <gael.lehchibi@outlook.com>
2020-05-25 16:23:24 +0200

    Correct structure project

--- blog-gateway/src/test/java/com/blog/gateway/GatewayApplicationTests.java
+++ blog-gateway/src/test/java/com/blog/gateway/GatewayApplicationTests.java
@@ -2,12 +2,17 @@ package com.blog.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
+@Rollback
 class GatewayApplicationTests {
 
     @Test
     void contextLoads() {
+        assertDoesNotThrow(() -> GatewayApplication.main(new String[]{}));
     }
 
 }
