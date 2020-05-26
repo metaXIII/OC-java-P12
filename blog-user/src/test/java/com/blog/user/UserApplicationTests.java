85d306e83786d9d0aa0266cbd5de91ea4ed8ca3c
meta <gael.lehchibi@outlook.com>
2020-05-26 21:59:29 +0200

    fix IT Test

--- blog-user/src/test/java/com/blog/user/UserApplicationTests.java
+++ blog-user/src/test/java/com/blog/user/UserApplicationTests.java
@@ -2,12 +2,17 @@ package com.blog.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
+@Rollback
 class UserApplicationTests {
 
     @Test
     void contextLoads() {
+        assertDoesNotThrow(() -> UserApplication.main(new String[]{}));
     }
 
 }
