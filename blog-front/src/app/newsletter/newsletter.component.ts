dade8b8bee2feeea809db81e9b826d5aa276f9ea
meta <gael.lehchibi@outlook.com>
2020-05-27 19:20:13 +0200

    add view for newsletter

---
+++ blog-front/src/app/newsletter/newsletter.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms"
+import {EmailService} from "../service/email.service"
+
+@Component({
+  selector   : 'app-newsletter',
+  templateUrl: './newsletter.component.html',
+  styleUrls  : ['./newsletter.component.scss']
+})
+export class NewsletterComponent implements OnInit {
+  subscriptionForm: FormGroup
+  private success: boolean
+  private error: boolean
+
+  constructor(private formBuilder: FormBuilder, private emailService: EmailService) {
+  }
+
+  ngOnInit() {
+    this.initForm()
+  }
+
+  Submit() {
+    let formValue = this.subscriptionForm.value
+    this.emailService.add(formValue).subscribe(resp => {
+      this.success = true
+    }, err => {
+      this.error = true
+    })
+  }
+
+  private initForm() {
+    this.subscriptionForm = this.formBuilder.group({
+      email: ['', Validators.required]
+    })
+  }
+}
