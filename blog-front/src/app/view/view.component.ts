097204ea60357c28369630819d98f5afad8e9ad7
meta <gael.lehchibi@outlook.com>
2020-05-27 15:48:19 +0200

    Add view component for article

---
+++ blog-front/src/app/view/view.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router"
+import {Article} from "../models/article.model"
+import {ArticleService} from "../service/article.service"
+
+@Component({
+  selector   : 'app-view',
+  templateUrl: './view.component.html',
+  styleUrls  : ['./view.component.scss']
+})
+export class ViewComponent implements OnInit {
+  id: number
+  article: Article = {
+    titre    : '',
+    content  : '',
+    synopsis : '',
+    localDate: new Date()
+  }
+
+  constructor(private route: ActivatedRoute, private articleService: ArticleService) {
+  }
+
+  ngOnInit() {
+    this.id = this.route.snapshot.params['id']
+    this.articleService.findById(this.id).subscribe((resp: Article) => {
+      this.article = resp
+    }, err => {
+      console.log(err)
+    })
+  }
+
+}
